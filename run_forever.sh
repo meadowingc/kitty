fb6a1c4457c08d70573b6aad215dd78410e30f37
Meadow <meadowingc@gmail.com>
2025-08-13 16:42:53 -0600

    tailwind in forever

--- run_forever.sh
+++ run_forever.sh
@@ -3,6 +3,7 @@
 # run forever, even if we fail
 while true; do
     git pull
+    ./tailwindcss -i assets/css/tailwind.css -o assets/css/main.css --minify
     go build -tags release -o kitty
     ./kitty
     sleep 1
